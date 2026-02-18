c69b7c1b93ae398238ce8e23cb20d22504fa7bdc
KatarinaVasic <amk1011817@student.hamk.fi>
2026-02-18 18:42:52 +0200

    Update db_connect.php

--- WebDevProject_CNSS/WebDevProject_CNSS-1/db_connect.php
+++ WebDevProject_CNSS/WebDevProject_CNSS-1/db_connect.php
@@ -1,14 +1,12 @@
 <?php
-$servername = "db"; // Replace with your MySQL server hostname
-$username = "admin";     // Replace with your MySQL username
-$password = "Password123";     // Replace with your MySQL password
-$dbname = "cnsstech_db";       // Replace with the name of your MySQL database
+$host = "db";  
+$port = 3306;                     
+$user = "root";
+$pass = "password";
+$dbname = "Users";
 
-// Create a database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($host, $user, $pass, $dbname, $port);
 
-// Check connection
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die("Connection failed");
 }
-?>
